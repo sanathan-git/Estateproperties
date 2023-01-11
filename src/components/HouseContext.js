9f6d0c67779932b384d141192a95ca8e4840ba30
unknown <{sanathan6522@gmail.com}>
2023-01-10 22:11:32 -0800

    first commit

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,134 @@
-import React from 'react';
+import React , {useState, useEffect, createContext} from 'react';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+// import data
+import { housesData } from '../data';
+
+//export context
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const[houses, setHouses] = useState(housesData);
+  const[country, setCountry] = useState('Location (any)');
+  const[countries, setCountries] = useState([]);
+  const[property, setProperty] = useState(['Property type (any)']);
+  const[properties, setProperties] = useState([]);
+  const[price, setPrice] = useState('Price range (any)');
+  const[loading, setLoading] = useState(false);
+
+  //return all countries
+  useEffect(() => {
+   const allCountries = houses.map((house) => {
+     return house.country;
+   });
+   //remove duplicates;
+   const uniqueCountries = ['Location (any)', ...
+   new Set(allCountries)] 
+
+   //set countries state
+   setCountries(uniqueCountries);
+  },[])
+  
+  //return all properties
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+    //remove duplicates;
+    const uniqueProperties = ['Location (any)', ...
+    new Set(allProperties)];
+ 
+    //set properties state
+    setProperties(uniqueProperties);
+   },[])
+     
+  const handleClick = () => {
+    //set loading
+    setLoading(true);
+    //create a function that checks if the string includes '(any)'
+    const isDefault = (str) => {
+      return str.split(' ').includes('(any)');
+    };
+    
+    // get final value of price and parse it to number
+
+    const minPrice = parseInt(price.split(' ')[0]);
+   //get the second value of price which is the maximum price $ parse it to number
+    const maxPrice = parseInt(price.split(' ')[2]);
+
+    const newHouses = housesData.filter((house)=> {
+      const housePrice = parseInt(house.price);
+
+      // If all values are selected
+      if (
+        house.country === country && 
+        house.type === property && 
+        housePrice >= minPrice && 
+        housePrice <= maxPrice )
+      {
+        return house;
+      }
+      // if all values are default
+      if(isDefault(country) && isDefault(property) && isDefault(price)){
+        return house;
+      }
+      // if country is not defined
+      if(!isDefault(country) && isDefault(property) && isDefault(price)){
+        return house.country === country;
+      }
+       // if property is not defined
+      if(!isDefault(property) && isDefault(country) && isDefault(price)){
+        return house.type === property;
+      }
+      // if price is not defined
+      if(!isDefault(price) && isDefault(country) && isDefault(property)){
+        if(housePrice >= minPrice && housePrice <= maxPrice){
+          return house;
+        }
+      }
+      // if country & property are default
+      if(!isDefault(country) && !isDefault(property) && isDefault(price)){
+        return house.country === country && house.type === property;
+      }
+      // if country and price is not default
+      if(!isDefault(country) && isDefault(property) && !isDefault(price)){
+        if(housePrice >= minPrice && housePrice <= maxPrice) {
+          return house.country === country;
+        }
+      }
+      
+      // property and price is not default
+      if(!isDefault(country) && !isDefault(property) && !isDefault(price)){
+        if(housePrice >= minPrice && housePrice <= maxPrice){
+          return house.type === property;
+        }
+    }
+  });
+
+  setTimeout(()=> {
+     return (
+      newHouses.length < 1 ? setHouses([]) : 
+      setHouses(newHouses),
+      setLoading(false)
+     );
+  }, 1000);
 };
 
-export default HouseContext;
+  return (
+    <HouseContext.Provider value={{ 
+      country,
+      setCountry,
+      countries,
+      property,
+      setProperty,
+      properties,
+      price,
+      setPrice,
+      houses,
+      loading,
+      handleClick,
+      loading,
+   }}>{children}</HouseContext.Provider>
+  )
+};
+
+export default HouseContextProvider;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+//import router
+import { BrowserRouter as Router }  from 'react-router-dom'
+import HouseContextProvider from './components/HouseContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<HouseContextProvider>  
+  <Router>
+   <React.StrictMode>
     <App />
-  </React.StrictMode>
+   </React.StrictMode>
+  </Router>
+</HouseContextProvider>
 );
+
